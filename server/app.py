f485f73c89c98102fd8dfb3a4499f424d6d4e877
bravinkibetnyangusei <bravink599@gmail.com>
2024-07-02 12:38:42 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
